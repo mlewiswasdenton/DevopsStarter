7ad960245e7d3b0abd5e3ee902ac1f348e307d28
Michelle Denton <michelle.denton@asda.co.uk>
2023-11-08 15:06:33 +0000

    Fixed .venv issues

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
+from todo_app.data.session_items import get_items
 
 from todo_app.flask_config import Config 
-from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
+
 @app.route('/')
 def index():
     items = get_items( )
-    return render_template('index.html', items = get_items)
+    return render_template('index.html', items = items)

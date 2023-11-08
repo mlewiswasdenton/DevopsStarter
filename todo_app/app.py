b08fbf3861bd353f2f13ffeff23c53427dc16bdf
Michelle Denton <michelle.denton@asda.co.uk>
2023-11-08 13:50:43 +0000

    show list of items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
-from todo_app.flask_config import Config
+from todo_app.flask_config import Config 
+from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
 def index():
-    return render_template('hello.html')
+    items = get_items( )
+    return render_template('index.html', items = get_items)

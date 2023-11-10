1a8ebe62b526359018c44941c8c464aa33c6724a
Michelle Denton <michelle.denton@asda.co.uk>
2023-11-10 15:50:19 +0000

    Add new item

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config 
 
@@ -9,5 +9,11 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items( )
+    items = get_items()
     return render_template('index.html', items = items)
+
+@app.route('/add-todo', methods = ["POST"])
+def add_todo():
+    new_todo_title = request.form.get('title')
+    add_item(new_todo_title)
+    return redirect('/')
